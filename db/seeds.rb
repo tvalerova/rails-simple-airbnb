64dfdc57121bee07cf5ce4cbd1a3e180bf92daa3
Tereza Valerova <terezavalerova@gmail.com>
2020-02-19 16:36:07 +0100

    model, seeds, index controller created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Flat.destroy_all
+10.times do
+  flat = Flat.create!(
+    name: Faker::Company.catch_phrase,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 7),
+    price_per_night: rand(100..200),
+    number_of_guests: rand(1..10)
+  )
+  flat.save
+end
